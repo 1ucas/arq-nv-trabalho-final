486de69db1d7311900c454c5ceb2940a27e5b603
Lucas <lucasramos.m@gmail.com>
2018-12-31 14:26:41 -0200

    Incio Microserviço de Localização

---
+++ Locations.Microservice/src/main/java/self/locationms/Application.java
@@ -0,0 +1,12 @@
+package self.locationms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
